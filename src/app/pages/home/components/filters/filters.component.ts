b707ca32b4b950445e162314cd56d863e1fe8af6
Peter Ahlgren <ahlgren1234@gmail.com>
2022-10-13 11:35:38 +0200

    Implement store api

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,19 +1,41 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showcategory = new EventEmitter<string>();
 
-  categories = ['shoes', 'sports'];
+  categoriesSubscription: Subscription | undefined;
 
-  constructor() {}
+  categories: Array<string> | undefined;
 
-  ngOnInit(): void {}
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService
+      .getAllcategories()
+      .subscribe((response) => {
+        this.categories = response;
+      });
+  }
 
   onShowCategory(category: string): void {
     this.showcategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
